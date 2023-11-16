35090a6936b381e3f39dff4966e82379b1e3e875
aristo-13 <oseirichmond310@gmail.com>
2023-11-16 23:06:39 +0000

    new

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,53 @@
+import paymentMethods from '/images/Tiny-Payment-Method-Icons.webp'
 
 function Footer() {
   return (
     <div>
-      footer
+      <div className='grid grid-cols-4'>
+        <div className='flex flex-col'>
+          <h1>Categories</h1>
+          <span>Men</span>
+          <span>Women</span>
+          <span>Shoes</span>
+          <span>Accesories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className='flex flex-col'>
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cooies</span>
+        </div>
+        <div>
+          <h1>About</h1>
+          <p>Lorem, ipsum dolor sit amet 
+            consectetur adipisicing elit. 
+            Vero dolorum soluta fuga deleniti est ad
+            architecto unde odio, recusandae dolor.
+            Illum magnam pariatur voluptatibus! Sed 
+            unde ex suscipit a eos!</p>
+        </div>
+        <div>
+        <h1>Contact</h1>
+          <p>Lorem, ipsum dolor sit amet 
+            consectetur adipisicing elit. 
+            Vero dolorum soluta fuga deleniti est ad
+            architecto unde odio, recusandae dolor.
+            Illum magnam pariatur voluptatibus! Sed 
+            unde ex suscipit a eos!</p>
+        </div>
+      </div>
+      <div className='flex items-center justify-between'>
+        <div>
+          <span>RICHSTORE</span>
+          <span>&copy; copyright 2023 All right reserved</span>
+        </div>
+        <div>
+          <img src={paymentMethods} width='300'/>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,16 @@
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import EN from '/images/EN.png'
 import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div>
-      <div className='flex justify-between'>
-         <div className='flex'>
+    <div className='border p-3 text-[18px]'>
+      <div className='flex justify-between items-center'>
+         <div className='flex items-center gap-[25px]'>
             <div className='flex items-center'>
                 <img src={EN} alt="logo" width='20' height='10'/>
                 <ExpandMoreIcon />
@@ -16,13 +20,29 @@ function Header() {
                 <ExpandMoreIcon />
             </div>
 
-            <Link to='category/1'>Men</Link>
-            <Link to='category/2'>Women</Link>
-            <Link to='category/3'>Children</Link>
-            <Link to='category/4'>Accessories</Link>
+            <Link to='products/1'>Men</Link>
+            <Link to='products/2'>Women</Link>
+            <Link to='products/3'>Children</Link>
+            <Link to='products/4'>Accessories</Link>
          </div>
-         <div></div>
-         <div></div>
+         <div>
+            <Link className='text-[30px] font-medium' to='/'>RICHSTORE</Link>
+         </div>
+         <div className='flex gap-[25px] items-center cursor-pointer'>
+            <Link to='/'>Homepage</Link>
+            <Link to='/'>About</Link>
+            <Link to='/'>Contact</Link>
+            <Link to='/'>Stores</Link>
+            <div className='flex items-center gap-2'>
+               <SearchIcon fontSize='small'/>
+               <PersonOutlineIcon fontSize='small'/>
+               <FavoriteBorderOutlinedIcon fontSize='small'/>
+               <div className='relative cursor-pointer text-[12px]'>
+                 <ShoppingCartOutlinedIcon fontSize='small'/>
+                 <span className='absolute -right-1 -top-2 w-4 h-4 bg-blue-500 flex justify-center items-center rounded-full text-white'>0</span>
+               </div>
+            </div>
+         </div>
       </div>
     </div>
   )
